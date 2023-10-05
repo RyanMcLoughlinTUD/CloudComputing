61bd062d6cbb8fed02535abf55bc0078cac9270f
RyanMcLoughlinTUD <C21431604@mytudublin.ie>
2023-10-05 15:33:57 +0100

    Button added

--- getting-started-app/src/static/js/app.js
+++ getting-started-app/src/static/js/app.js
@@ -175,7 +175,3 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
         </Container>
     );
 }
-
-function redirectUser(){
-	window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}
---
+++ getting-started-app/src/static/js/redirect.js
@@ -0,0 +1,5 @@
+// redirect.js
+
+function redirectToURL(url) {
+    window.location.href = url;
+}
